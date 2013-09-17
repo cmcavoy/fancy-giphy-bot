84b9fad7ae20e0812c75e013bac074bd2fce4dad
Chris McAvoy <chris.mcavoy@gmail.com>
2013-09-17 17:50:09 -0500

    hrm.

---
+++ app.js
@@ -0,0 +1,57 @@
+const GROUPMETOKEN = process.env['GROUPMETOKEN'];
+const GIPHYTOKEN = process.env['GIPHYTOKEN'];
+const GROUP = process.env['GROUP'];
+const URL = process.env['URL'];
+const natural = require('natural');
+const _ = require('underscore');
+const util = require('util');
+const request = require('request');
+
+var tokenizer = new natural.WordTokenizer();
+
+var giphy = require('giphy-wrapper')(GIPHYTOKEN);
+var bot = require('fancy-groupme-bot')(
+  { token:GROUPMETOKEN,
+    name: "giphybot",
+    group: GROUP,
+    url: URL });
+
+bot.on('botRegistered', function() {
+  console.log("online");
+});
+
+bot.on('botMessage', function(bot, message) {
+  console.log('incoming');
+  if (message.name != 'giphybot') {
+    var tokens = tokenizer.tokenize(message.text);
+
+    tokens = _.map(tokens, function(t) { return t.toLowerCase(); });
+
+    if (tokens.indexOf('giphybot') >= 0) {
+      tokens = _.without(tokens, 'giphybot');
+      console.log("searching for " + tokens);
+      giphy.search(tokens.toString(), 1, 0, function(err, data) {
+        if (err) console.error(err);
+        console.log("giphy returned " + util.inspect(data));
+        if (data.data.length) {
+          var id = data.data[0].id;
+          var imageUrl = "http://media3.giphy.com/media/" + id + "/giphy.gif";
+          console.log(util.inspect(data.data));
+          console.log("image url " + imageUrl);
+          request(imageUrl).pipe(request.post('https://image.groupme.com/pictures',
+                                              function(err, response, body) {
+                                                if (err) console.log("error " + err);
+                                                console.log("response " + util.inspect(response));
+                                                console.log("got a groupme image back " + body);
+                                                bot.message(body);
+                                              })
+                                );
+
+        }
+      });
+    }
+
+  };
+});
+
+bot.serve(3000);
